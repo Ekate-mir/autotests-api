5456b5937c0037606e0d2d3319b5650e3d3d2c59
Ekate-mir <e_mirgorodskaya08@list.ru>
2026-06-26 15:13:28 +0500

    Замена conftest на плагины

--- fixtures/users.py
+++ fixtures/users.py
@@ -1,4 +1,3 @@
-from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
 from clients.users.public_users_client import PublicUsersClient, get_public_users_client
@@ -22,11 +21,6 @@ class UserFixture(BaseModel):
     @property
     def authentication_user(self) -> AuthenticationUserSchema:
         return AuthenticationUserSchema(email=self.request.email, password=self.request.password)
-
-
-@pytest.fixture
-def authentication_client() -> AuthenticationClient:
-    return get_authentication_client()
 
 
 @pytest.fixture
