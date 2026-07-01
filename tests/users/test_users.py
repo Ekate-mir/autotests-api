e8964c933cb19f90cdb8e4a24f6a42dff7b93d74
Ekate-mir <e_mirgorodskaya08@list.ru>
2026-07-01 19:23:15 +0500

    Интеграция Allure в автотесты. Практическая работа с Allure аннотациями

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -21,12 +21,15 @@ import allure
 @allure.tag(AllureTag.USERS, AllureTag.REGRESSION)
 @allure.epic(AllureEpic.LMS)
 @allure.feature(AllureFeature.USERS)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.USERS)
 class TestUsers:
     @pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "example.com"])
     @allure.tag(AllureTag.CREATE_ENTITY)
     @allure.story(AllureStory.CREATE_ENTITY)
     @allure.title("Create user")
     @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.CREATE_ENTITY)
     def test_create_user(self, domain: str, public_users_client: PublicUsersClient):
         request = CreateUserRequestSchema(email=fake.email(domain))
         response = public_users_client.create_user_api(request)
@@ -41,6 +44,7 @@ class TestUsers:
     @allure.story(AllureStory.GET_ENTITY)
     @allure.title("Get user me")
     @allure.severity(Severity.CRITICAL)
+    @allure.sub_suite(AllureStory.GET_ENTITY)
     def test_get_user_me(self,
                          function_user: UserFixture,
                          private_users_client: PrivateUsersClient
